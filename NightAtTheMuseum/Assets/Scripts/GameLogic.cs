bf5201894427f523d873b21c718ff9f5cedbb244
nickcottrell <nick.a.cottrell@gmail.com>
2018-01-11 23:43:16 -0800

    got project set up, successfully deployed a test to my phone

---
+++ NightAtTheMuseum/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GoToScene(string SceneName) {
+		Debug.Log ("My method was called.");
+		SceneManager.LoadScene (SceneName);
+	}
+}
